889bfda2f3ce56202b7ff2229af8eb520ab34d0b
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-24 13:47:08 -0800

    create new files for toDo, first test
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>
    Co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

---
+++ js/scriptsToDo.js
@@ -0,0 +1,7 @@
+// Business logic for Places
+function Task(description, dueDate, owners, isComplete) {
+  this.description = description;
+  this.dueDate = dueDate;
+  this.owners = owners;
+  this.isComplete = isComplete;
+}
